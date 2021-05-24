518ec4c529f62de7ea223e3e0f0edf3abc8a7d24
Буков Александр Викторович (DigitalSpirit) <olegthander@yander.ru>
2021-05-24 18:35:59 +0300

    добавил юзер интерфейс для запуска, получения ip и закрытия бэка

--- src/main/java/com/dungeonmaster/DungeonMasterApplication.java
+++ src/main/java/com/dungeonmaster/DungeonMasterApplication.java
@@ -3,10 +3,15 @@ package com.dungeonmaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ui.UserInterface;
+
 @SpringBootApplication
 public class DungeonMasterApplication {
 
 	public static void main(String[] args) {
+		UserInterface ui = new UserInterface();
+        ui.pack();
+        
 		SpringApplication.run(DungeonMasterApplication.class, args);
 	}
 
--- src/main/java/com/dungeonmaster/controller/UserController.java
+++ src/main/java/com/dungeonmaster/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dungeonmaster.errros.user.InvalidUserData;
-import com.dungeonmaster.modelDto.user.LoginForm;
 import com.dungeonmaster.modelDto.user.UserDTO;
 import com.dungeonmaster.service.user.UserService;
 
