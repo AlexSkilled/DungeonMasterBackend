d18e60f091b36b36cd1d23a8b07c8b06d93665de
Буков Александр Викторович (DigitalSpirit) <olegthander@yander.ru>
2021-05-27 11:29:35 +0300

    cummit

--- src/main/java/com/dungeonmaster/DungeonMasterApplication.java
+++ src/main/java/com/dungeonmaster/DungeonMasterApplication.java
@@ -3,15 +3,10 @@ package com.dungeonmaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ui.UserInterface;
-
 @SpringBootApplication
 public class DungeonMasterApplication {
 
 	public static void main(String[] args) {
-		UserInterface ui = new UserInterface();
-        ui.pack();
-        
 		SpringApplication.run(DungeonMasterApplication.class, args);
 	}
 
